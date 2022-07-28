4b4f17c49737c5f1e30eba73c7d32760f3155abb
TakayukiKomada <Takakoma.79@gmail.com>
2022-07-28 13:00:12 +0900

    ペットショップ2課題完了

--- server/php_exercise02/pet_shop/index.php
+++ server/php_exercise02/pet_shop/index.php
@@ -1,7 +1,10 @@
 <?php
 require_once __DIR__ . '/functions.php';
 
-$get_info = get_info();
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    $keyword = filter_input(INPUT_GET, 'keyword');
+    $search_info = get_info($keyword);
+}
 
 ?>
 
@@ -16,9 +19,13 @@ $get_info = get_info();
 </head>
 
 <body>
-    <h2>本日のご紹介ペット！</h2>
+    <h2>本日のご紹介ペット!</h2>
+    <form action="" method="get">キーワード:
+        <input type="search" name="keyword" placeholder="キーワードの入力">
+        <input type="submit" value="検索">
+    </form>
     <ul>
-        <?php foreach ($get_info as $animal) : ?>
+        <?php foreach ($search_info as $animal) : ?>
             <?= h($animal['type'] . "の" . $animal['classification'] . "ちゃん")  ?><br>
             <?= h($animal['description'])  ?><br>
             <?= h($animal['birthday'] . "生まれ")  ?><br>
